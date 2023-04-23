7a5f96636aa0ef9e6f94371cd2c09187719800f8
Satyankar Pariki <satyankar07@gmail.com>
2023-04-23 15:18:54 +0530

    changes ig

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import { Route } from 'react-router-dom';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar';
-import Podcast from './components/Podcast/Podcast';
+
 function App() {
   return (
     <Routes>
       <Route path="/" component={<Home />} />
-      <Route path="/Podcast" component={<Podcast />} />
     </Routes>
   );
 }
---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { useState } from "react";
+import "./Admin.css";
+
+export function Admin(props) {
+  const [podcastName, setPodcastName] = useState("");
+  const [speakerName, setSpeakerName] = useState("");
+  const [podcastDescription, setPodcastDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [language, setLanguage] = useState("");
+  const [fileType, setFileType] = useState("audio");
+  // const [file, setFile] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+  };
+
+  // const handleFileChange = (event) => {
+  //   setFile(event.target.files[0]);
+  // };
+
+  return (
+    <>
+      <div>
+        <h1>+Add Podcast+</h1>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="namediv">
+            <label className="Name">Enter Podcast Name: </label>
+            <input
+              className="Name"
+              type="text"
+              value={podcastName}
+              size="20"
+              onChange={(e) => setPodcastName(e.target.value)}
+            />
+          </div>
+          <div className="descdiv">
+            <label className="Description">Enter Description: </label>
+            <textarea
+              className="Description"
+              value={podcastDescription}
+              onChange={(e) => setPodcastDescription(e.target.value)}
+            />
+          </div>
+          <div className="speakdiv">
+            <label className="Speaker">Enter Speaker: </label>
+            <input
+              className="SpeakerName"
+              value={speakerName}
+              type="text"
+              onChange={(e) => setSpeakerName(e.target.value)}
+            />
+          </div>
+          <div className="catediv">
+            <label className="Category">Select Category: </label>
+            <select
+              className="Categorys"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">~</option>
+              <option value="Technology">Technology </option>
+              <option value="Science">Science </option>
+              <option value="Sports">Sports </option>
+              <option value="Music">Music </option>
+              <option value="News">News </option>
+              <option value="Other">Other...</option>
+            </select>
+          </div>
+          <div className="langdiv">
+            <label className="Language">Select Language: </label>
+            <select
+              className="Languages"
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)}
+            >
+              <option value="English">English</option>
+              <option value="Spanish">Español</option>
+              <option value="French">Français</option>
+              <option value="Japanese">日本</option>
+              <option value="Tamil">தமிழ்</option>
+              <option value="Telugu">తెలుగు</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className="typediv">
+            <label className="Type">Choose Podcast Type: </label>
+            <radio>
+              <span>
+                <label>Audio</label>
+                <input
+                  type="radio"
+                  name="fileType"
+                  value="audio"
+                  checked={fileType === "audio"}
+                  onChange={(e) => setFileType(e.target.value)}
+                />
+              </span>
+              <span>
+                <label>Video</label>
+                <input
+                  type="radio"
+                  name="fileType"
+                  value="video"
+                  checked={fileType === "video"}
+                  onChange={(e) => setFileType(e.target.value)}
+                />
+              </span>
+            </radio>
+          </div>
+          {/* <div>
+            <label>Choose File:</label>
+            <input
+              type="file"
+              accept={fileType === "audio" ? "audio/*" : "video/*"}
+              onChange={handleFileChange}
+            />
+          </div> */}
+          <div className="submitdiv">
+            <input className="submit" type="submit" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+export default Admin;
